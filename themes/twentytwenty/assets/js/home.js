8e0f7bcc31dd677ac43a61eb2dbdf6f72be11d2c
Your Name <you@example.com>
2020-01-21 09:41:18 +0100

    WIP, structure Ok, barely any CSS

---
+++ themes/twentytwenty/assets/js/home.js
@@ -0,0 +1,15 @@
+// document.getElementsByClassName("banner").forEach(element => {
+//     element.addEventListener("mouseover", () => {
+//         let bannerMore = document.getElementsByClassName("banner-more")[0];
+//         let title = document.getElementsByTagName("h2")[0];
+//         let desc = document.createElement("p");
+//         // let link = document.createElement("a");
+
+//         title.innerHTML = "new title";
+//         bannerMore.appendChild(desc);
+//         // bannerMore.appendChild(link);
+//         desc.innerHTML = "Lorem ipsum maggle bigulum";
+//         // link.innerHTML = "<?php the_field('hover_link_text'); ?>";
+//         // link.setAttribute("href", "<?php the_field('hover_link'); ?>");
+//     });
+// });
---
+++ themes/twentytwenty/page-home.php
@@ -0,0 +1,181 @@
+<?php
+/*
+Template Name: Home
+*/
+?>
+
+<?php
+
+global $siteUrl;
+function mypage_head() {
+    echo '<link rel="stylesheet" type="text/css" href="'.get_bloginfo('stylesheet_directory').'/assets/css/home.css">'."\n";
+    echo '<script defer src="'.get_bloginfo('stylesheet_directory').'/assets/js/home.js"></script>'."\n";
+    }
+    add_action('wp_head', 'mypage_head');
+?>
+
+<?php get_header(); ?>
+
+<div id="banners">
+
+    <?php
+
+    // check if the repeater field has rows of data
+    if( have_rows('banner') ):
+
+        // loop through the rows of data
+        while ( have_rows('banner') ) : the_row();
+
+        $image = get_sub_field('image'); 
+        
+        ?>
+
+        <div class="banner">
+            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+
+            <div class="banner-standard">
+                <h2><?php the_sub_field('title'); ?></h2>
+            </div>
+
+            <div class="banner-detailed">
+                <h2><?php the_sub_field('hover_title'); ?></h2>
+                <p><?php the_sub_field('hover_description'); ?></p>
+                <a href="<?php the_sub_field('hover_link'); ?>"><?php the_sub_field('hover_link_text'); ?></a>
+            </div>
+        </div>
+
+        <?php
+
+        endwhile;
+
+    else :
+        // no rows found
+    endif;
+    ?>
+</div>
+
+
+
+<!-- <?php
+if( have_rows('banner') ): while ( have_rows('banner') ) : the_row(); 
+?>
+
+    <div id="banner">
+
+        <?php
+        if( have_rows('repeater') ): while ( have_rows('repeater') ) : the_row();       
+        $image = get_sub_field('image'); 
+        ?>
+
+            <div class="banner">
+                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+
+                <div class="banner-standard">
+                    <h2><?php the_sub_field('title'); ?></h2>
+                </div>
+
+                <div class="banner-detailed">
+                    <h2><?php the_sub_field('hover_title'); ?></h2>
+                    <p><?php the_sub_field('hover_text'); ?></p>
+                    <a href="<?php the_sub_field('hover_link'); ?>"><?php the_sub_field('hover_link_text'); ?></a>
+                </div>
+            </div>
+
+        <?php
+        endwhile; endif;
+        ?>
+
+    </div>
+
+<?php 
+endwhile; endif; 
+?> -->
+
+<div id="content">
+
+    <?php
+    $info_bubble_black = get_field('info_bubble_black');
+    if( $info_bubble_black ): ?>
+        <div id="info_bubble_black">
+            <p><strong><?php echo esc_html($info_bubble_black['bold_text']); ?></strong><?php echo esc_html($info_bubble_black['normal_text']); ?></p>
+        </div>
+    <?php endif; ?>
+
+    <?php
+    $info_bubble_white = get_field('info_bubble_white');
+    if( $info_bubble_white ): ?>
+        <div id="info_bubble_white">
+            <h2><?php echo esc_html($info_bubble_white['title']); ?></h2>
+            <p><?php echo $info_bubble_white['text']; ?></p>
+            <a href="<?php echo esc_url($info_bubble_white['link']); ?>">Le magasin</a>
+        </div>
+    <?php endif; ?>
+
+    <?php
+    $vertical_image = get_field('vertical_image');
+    ?>
+        <div id="vertical_image">
+            <img src="<?php echo $vertical_image['url']; ?>" alt="<?php echo $vertical_image['alt']; ?>">
+        </div>
+    <?php ?>
+
+    <?php
+    $info_bubble_mixed = get_field('info_bubble_mixed');
+    if( $info_bubble_mixed ): ?>
+        <div id="info_bubble_mixed">
+            <img src="<?php echo esc_url($info_bubble_mixed['logo']['url']); ?>" alt="<?php echo esc_html($info_bubble_mixed['logo']['alt']); ?>">
+            <p><?php echo esc_html($info_bubble_mixed['text']); ?></p>
+            <a href="<?php echo esc_url($info_bubble_mixed['link']); ?>">Services d'archi d'intérieur</a>
+        </div>
+    <?php endif; ?>
+
+    <?php
+    $article = get_field('article');
+    if( $article ): ?>
+        <div id="article">
+            <img src="<?php echo esc_url($article['image']['url']); ?>" alt="<?php echo esc_html($article['image']['alt']); ?>">
+            <h2><?php echo esc_html($article['title']); ?></h2>
+            <p><?php echo $article['text']; ?></p>
+            <a href="<?php echo esc_url($article['link']); ?>">Tapisserie d'ameublement</a>
+        </div>
+    <?php endif; ?>
+
+</div>
+
+
+<!-- <div id="content">
+
+    <div id="info-bubble">
+        <p><strong><?php the_field('info_bubble_catch_phrase'); ?></strong><?php the_field('info_bubble_text'); ?></p>
+    </div>
+    <div id="info-bubble-2">
+        <h3><?php the_field('info_bubble_2_title'); ?></h3>
+        <p><?php the_field('info_bubble_2_text'); ?></p>
+        <a href="<?php the_field('info_bubble_2_link'); ?>">Le magasin<img src="" alt=""></a>
+    </div>
+
+    <?php $image = get_field('vertical_image'); ?>
+    <img id="vertical-image" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+
+    <div id="catch-phrase">
+
+        <?php $logo = get_field('catch_phrase_logo'); ?>
+        <img src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>">
+
+        <h4><?php the_field('catch_phrase_title'); ?></h4>
+        <a href="<?php the_field('catch_phrase_link'); ?>">Services d'archi d'intérieur<img src="" alt=""></a>
+    </div>
+    <div id="home-article-image">
+
+        <?php $image = get_field('home_article_image') ?>
+        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+    </div>
+    <div id="home-article-content">
+        <h3><?php the_field('home_article_title'); ?></h3>
+        <p><?php the_field('home_article_text'); ?></p>
+        <a href="<?php the_field('home_article_link'); ?>">Tapisserie d'ameublement<img src="" alt=""></a>
+    </div>
+
+</div> -->
+
+<?php get_footer(); ?>
